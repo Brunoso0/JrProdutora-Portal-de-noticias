2b0ef335b37424d551e6a1c744b69417fe46860e
Brunoso0 <bruno.santosbrito@outlook.com>
2025-04-25 17:00:29 -0300

    responsive ready 3

--- src/components/SecaoInscricao.jsx
+++ src/components/SecaoInscricao.jsx
@@ -1,34 +1,39 @@
 import FormularioInscricao from "./FormularioInscricao";
 
-const SecaoInscricao = () => (
-  <section className="secao-inscricao">
-    <img src="/img/fundo-amarelo.png" alt="Fundo amarelo" className="fundo-costura desktop-amarelo" />
-    <img src="/img/fundo-amarelo-gigante.png" alt="Fundo amarelo" className="fundo-costura mobile-amarelo"  />
-    <img src="/img/fundo-amarelo-grande.png" alt="Fundo amarelo" className="fundo-costura tablet-amarelo"  />
-    
-    <div className="conteudo-inscricao">
+const SecaoInscricao = () => {
+  const imagens = [
+    { src: "/img/fundo-amarelo.png", alt: "Fundo amarelo", className: "fundo-costura desktop-amarelo" },
+    { src: "/img/fundo-amarelo-gigante.png", alt: "Fundo amarelo", className: "fundo-costura mobile-amarelo" },
+    { src: "/img/fundo-amarelo-grande.png", alt: "Fundo amarelo", className: "fundo-costura tablet-amarelo" },
+  ];
+
+  const decoracoes = [
+    { src: "/img/balao.png", alt: "Balão decorativo", className: "decoracao-direita" },
+    { src: "/img/balao.png", alt: "Balão decorativo", className: "decoracao-direita-baixo" },
+    { src: "/img/balao.png", alt: "Balão decorativo", className: "decoracao-esquerda" },
+    { src: "/img/balao.png", alt: "Balão decorativo", className: "decoracao-esquerda-baixo" },
+  ];
+
+  return (
+    <section className="secao-inscricao">
+      {imagens.map((imagem, index) => (
+        <img key={index} src={imagem.src} alt={imagem.alt} className={imagem.className} />
+      ))}
+
+      <div className="conteudo-inscricao">
         <div className="titulo-inscricao">
           <img src="/img/inscrevase.png" alt="Título Inscreva-se" />
         </div>
 
-        <div className="decoracao-direita">
-          <img src="/img/balao.png" alt="Balão decorativo" />
-        </div>
-
-        <div className="decoracao-direita-baixo">
-          <img src="/img/balao.png" alt="Balão decorativo" />
-        </div>
-
-        <div className="decoracao-esquerda">
-          <img src="/img/balao.png" alt="Balão decorativo" />
-        </div>
-
-        <div className="decoracao-esquerda-baixo">
-          <img src="/img/balao.png" alt="Balão decorativo" />
-        </div>
+        {decoracoes.map((decoracao, index) => (
+          <div key={index} className={decoracao.className}>
+            <img src={decoracao.src} alt={decoracao.alt} />
+          </div>
+        ))}
 
         <FormularioInscricao />
-    </div>
-  </section>
-);
+      </div>
+    </section>
+  );
+};
 export default SecaoInscricao;
