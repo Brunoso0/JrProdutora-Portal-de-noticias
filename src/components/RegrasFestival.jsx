4ea3a60b1e0fb907c9447cba3ceb45ae0fb12b85
Brunoso0 <bruno.santosbrito@outlook.com>
2025-05-02 15:09:23 -0300

    Refactor code structure for improved readability and maintainability

--- src/components/RegrasFestival.jsx
+++ src/components/RegrasFestival.jsx
@@ -1,6 +1,6 @@
 const RegrasFestival = () => (
     <section className="regras-festival">
-      <img src="/img/data.png" alt="Data do evento" />
+      <img src="/img/data2.png" alt="Data do evento" />
       <img src="/img/informativo.png" alt="Informações do evento" />
     </section>
   );
